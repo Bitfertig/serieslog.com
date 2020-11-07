bdfeda504ab0f5d14236e77878d801e2cf8abae3
Benjamin Hermand <benjamin@hermand.de>
2020-11-07 12:45:09 +0100

    👌 IMPROVE:  logo svg

--- src/vue/icons.vue
+++ src/vue/icons.vue
@@ -1,6 +1,23 @@
 <template>
     <div>
         <!-- Icons -->
+        <svg v-if="type=='logo'" version="1.0" xmlns="http://www.w3.org/2000/svg" width="80px" height="112px" viewBox="0 0 89.000000 127.000000" preserveAspectRatio="xMidYMid meet">
+            <g transform="translate(0.000000,127.000000) scale(0.100000,-0.100000)" fill="#333333" stroke="none">
+                <path d="M0 635 l0 -635 445 0 445 0 0 635 0 635 -445 0 -445 0 0 -635z m870
+                0 l0 -615 -425 0 -425 0 0 615 0 615 425 0 425 0 0 -615z"/>
+                <path fill="#e2506e" d="M318 1070 c-87 -26 -120 -73 -125 -180 -4 -64 -1 -82 19 -120 28 -55
+                47 -66 239 -140 107 -41 162 -69 183 -90 31 -30 31 -31 31 -139 0 -95 -3 -112
+                -20 -131 -26 -30 -92 -60 -132 -60 -27 0 -33 -4 -33 -21 0 -20 4 -21 55 -16
+                73 7 134 42 162 92 19 34 23 55 23 133 0 162 -19 182 -256 272 -205 78 -224
+                95 -224 202 0 113 47 168 145 168 28 0 35 4 35 20 0 17 -6 20 -37 19 -21 0
+                -50 -4 -65 -9z"/>
+                <path fill="#e2506e" d="M480 1061 c0 -15 10 -19 51 -23 89 -9 129 -55 129 -150 0 -55 1 -58
+                25 -58 24 0 25 3 25 60 0 93 -36 145 -119 175 -62 22 -111 20 -111 -4z"/>
+                <path fill="#e2506e" d="M182 350 c3 -50 9 -77 24 -97 30 -41 96 -74 159 -80 50 -5 55 -4 55
+                14 0 15 -11 21 -58 31 -31 6 -68 19 -81 28 -31 22 -51 73 -51 129 0 42 -2 45
+                -26 45 -26 0 -26 0 -22 -70z"/>
+            </g>
+        </svg>
         <!-- Caret left -->
         <svg v-if="type=='caret_left'" width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-caret-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M10 12.796L4.519 8 10 3.204v9.592zm-.659.753l-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"/>
@@ -27,12 +44,7 @@
             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
             <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
         </svg>
-<!--         <svg v-if="type=='add'" id="add" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-node-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" d="M11 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8zM6.025 7.5a5 5 0 1 1 0 1H4A1.5 1.5 0 0 1 2.5 10h-1A1.5 1.5 0 0 1 0 8.5v-1A1.5 1.5 0 0 1 1.5 6h1A1.5 1.5 0 0 1 4 7.5h2.025zM11 5a.5.5 0 0 1 .5.5v2h2a.5.5 0 0 1 0 1h-2v2a.5.5 0 0 1-1 0v-2h-2a.5.5 0 0 1 0-1h2v-2A.5.5 0 0 1 11 5zM1.5 7a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z"/>
-        </svg> -->
-<!--         <svg v-if="type=='add'" id="add" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" viewBox="0 0 640 640" width="640" height="640">
-            <defs><path d="M6.4 171.71L319.1 171.71L319.1 48.26L6.4 48.26L6.4 171.71ZM26.5 150.53L26.5 66.94L154.5 66.94L154.5 150.53L26.5 150.53ZM173.12 150.53L173.12 66.94L301.12 66.94L301.12 150.53L173.12 150.53Z" id="b1gucJOGp4"></path><path d="M320.9 326.08L633.6 326.08L633.6 202.62L320.9 202.62L320.9 326.08ZM340.99 304.9L340.99 221.31L468.99 221.31L468.99 304.9L340.99 304.9ZM487.62 304.9L487.62 221.31L615.62 221.31L615.62 304.9L487.62 304.9Z" id="h2cTk2ehCo"></path><path d="M6.4 591.74L320.9 591.74L320.9 360.19L6.4 360.19L6.4 591.74ZM28.74 568.26L28.74 484.29L152.19 484.29L152.19 568.26L28.74 568.26ZM28.74 465.98L28.74 382.08L152.19 382.08L152.19 465.98L28.74 465.98ZM175.36 568.26L175.36 484.29L298.88 484.29L298.88 568.26L175.36 568.26ZM175.36 465.98L175.36 382.08L298.88 382.08L298.88 465.98L175.36 465.98Z" id="cwnNobcTX"></path><path d="M130.69 266.3L195.01 320.32L195.01 277.89L299.2 277.89L299.2 254.72L195.01 254.72L195.01 212.29L130.69 266.3Z" id="c1Osf12agc"></path></defs><g><g><g><use xlink:href="#b1gucJOGp4" opacity="1" fill="#000000" fill-opacity="1"></use><g><use xlink:href="#b1gucJOGp4" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="1" stroke-opacity="0"></use></g></g><g><use xlink:href="#h2cTk2ehCo" opacity="1" fill="#000000" fill-opacity="1"></use><g><use xlink:href="#h2cTk2ehCo" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="1" stroke-opacity="0"></use></g></g><g><use xlink:href="#cwnNobcTX" opacity="1" fill="#000000" fill-opacity="1"></use><g><use xlink:href="#cwnNobcTX" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="1" stroke-opacity="0"></use></g></g><g><use xlink:href="#c1Osf12agc" opacity="1" fill="#000000" fill-opacity="1"></use><g><use xlink:href="#c1Osf12agc" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="1" stroke-opacity="0"></use></g></g></g></g>
-        </svg> -->
+        <!-- add -->
         <svg v-if="type=='add'" id="add"  width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-plus-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
             <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
