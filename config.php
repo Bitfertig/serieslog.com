47cfb35f1dd7a65381ac30ef9b9b34136db1a48a
Aurelian Hermand <aurel@hermand.de>
2020-11-08 17:07:03 +0100

    🐛 FIX: cookie lifetime

--- config.php
+++ config.php
@@ -1,6 +1,8 @@
 <?php
 
-session_start(['cookie_lifetime' => $_ENV['SESSION_LIFETIME'] ]);
+ini_set('session.cookie_lifetime', $_ENV['SESSION_LIFETIME']);
+ini_set('session.gc_maxlifetime', $_ENV['SESSION_LIFETIME']);
+session_start();
 
 $dbhost = $_ENV['DB_HOST'].':'.$_ENV['DB_PORT'];
 $dbname = $_ENV['DB_DATABASE'];
