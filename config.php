5288c60b2c9f1d973e99947223c223a97cdafb19
Benjamin Hermand <benjamin@hermand.de>
2020-11-17 22:41:06 +0100

    👌 IMPROVE: namechange

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 ini_set('session.cookie_lifetime', $_ENV['SESSION_LIFETIME']);
 ini_set('session.gc_maxlifetime', $_ENV['SESSION_LIFETIME']);
-ini_set('session.save_path', __DIR__.'/../sessions');
+//ini_set('session.save_path', __DIR__.'/../sessions');
 session_start();
 
 $dbhost = $_ENV['DB_HOST'].':'.$_ENV['DB_PORT'];
