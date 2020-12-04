6a628eb457ca16f1ffbab3cd0682f789ef2f3013
Aurelian Hermand <aurel@hermand.de>
2020-12-04 01:58:14 +0100

    env

--- .env.example.php
+++ .env.example.php
@@ -8,4 +8,7 @@ $_ENV = [
     'DB_USERNAME' => '',
     'DB_PASSWORD' => '',
     'SESSION_LIFETIME' =>  86400 * 30 * 6, // 6 Monate
+    'SESSION_SAVEPATH' => ini_get('session.save_path'),
+    'WORKER_KEY' => '',
+    'OMDB_APIKEY' => '', // http://www.omdbapi.com/
 ];
