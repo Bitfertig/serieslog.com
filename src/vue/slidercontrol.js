a4e16df5bca867e5c72690254e437d125ed7ede5
Benjamin Hermand <benjamin@hermand.de>
2020-12-08 21:13:18 +0100

    slider

--- src/vue/slidercontrol.js
+++ src/vue/slidercontrol.js
@@ -5,12 +5,23 @@
 var slidercontrol = {
     data: function () {
         return {
+            mousedowntimer_start: null,
+            slidercontrol_mousedowntimer: false,
             slidercontrol_dragging: false,
-            //slidercontrol_drag_x: 0,
             slidercontrol_circle_pos: 8,
         }
     },
     methods: {
+        mousedownTimer(){
+            var mousedowntimer_end = new Date();
+            // time difference in ms
+            var  mousedowntimer_timeDiff = mousedowntimer_end - this.mousedowntimer_start;
+            // get seconds
+            var seconds = Math.round( mousedowntimer_timeDiff % 100000000);
+        
+            $(".time").text(seconds);
+        },
+
         startDrag() {
             this.slidercontrol_dragging = true;
             this.slidercontrol_circle_pos = 8;
@@ -22,7 +33,6 @@ var slidercontrol = {
         doDrag(event) {
             if (this.slidercontrol_dragging) {
             this.slidercontrol_circle_pos = event.clientX - ( document.getElementById('circle_test').getBoundingClientRect().left + 42);
-            console.log(event.clientX);
             console.log(document.getElementById('circle_test').getBoundingClientRect().left);
             }
         }
